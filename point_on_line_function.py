420a08e13569efcbc9bd064e51ecda247e24fe59
Xiang Dai <dino@DinodeMBP.lan1>
2020-09-04 16:08:32 -0400

    edit the function which is able to pass all the test cases

---
+++ point_on_line_function.py
@@ -0,0 +1,16 @@
+# point_on_line_function
+def point_on_line(point1, point2, input):
+	x1 = point1[0]
+	y1 = point1[1]
+	x2 = point2[0]
+	y2 = point2[1]
+	if (x1 - x2) == 0:
+		return "NaN"
+	elif (y1 - y2) == 0:
+		return input
+	else:
+		k = (y2 - y1)/(x2 - x1)
+		b = y1 - k * x1
+		output = input * k + b
+		return output		
+		
