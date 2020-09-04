32b03737a6bd9e12f7c1878d34eb55983fdb5135
Xiang Dai <dino@DinodeMBP.lan1>
2020-09-04 16:25:37 -0400

    do not change the information but pass the style test

--- test_point_on_line_function.py
+++ test_point_on_line_function.py
@@ -1,13 +1,15 @@
 # test_point_on_line_function
 import pytest
+
+
 @pytest.mark.parametrize("point1, point2, input, expected", [
-	((1, 1), (2, 2), 3, 3),
-	((1, 0), (1, 2), 1, "NaN"),
-	((-1, 0), (0, 1), 1, 2),
-	((0, 1), (0, 2), 0, "NaN"),
-	((1, 0), (2, 0), 4, 4)
+    ((1, 1), (2, 2), 3, 3),
+    ((1, 0), (1, 2), 1, "NaN"),
+    ((-1, 0), (0, 1), 1, 2),
+    ((0, 1), (0, 2), 0, "NaN"),
+    ((1, 0), (2, 0), 4, 4)
 ])
 def test_point_on_line(point1, point2, input, expected):
-	from point_on_line_function import point_on_line
-	answer = point_on_line(point1, point2, input)
-	assert answer == expected
+    from point_on_line_function import point_on_line
+    answer = point_on_line(point1, point2, input)
+    assert answer == expected
