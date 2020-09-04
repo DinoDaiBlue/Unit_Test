747d7de7784581202b892b52c94094b56c1a9928
Xiang Dai <dino@DinodeMBP.lan1>
2020-09-04 16:41:30 -0400

    create a new test function for the extra work which will return true and false

--- test_point_on_line_function.py
+++ test_point_on_line_function.py
@@ -13,3 +13,17 @@ def test_point_on_line(point1, point2, input, expected):
     from point_on_line_function import point_on_line
     answer = point_on_line(point1, point2, input)
     assert answer == expected
+
+
+@pytest.mark.parametrize("point1, point2, input, expected", [
+    ((1, 1), (2, 2), (3, 3), "True"),
+    ((1, 0), (1, 2), (2, 4), "Flase"),
+    ((1, 0), (1, 2), (1, 3), "True"),
+    ((0, 1), (0, 3), (0, 5), "True"),
+    ((1, 0), (2, 0), (4, 0), "True"),
+    ((0, 0), (1, 2), (2, 4), "True")
+])
+def test_point_on_line_tf(point1, point2, input, expected):
+    from point_on_line_function import point_on_line_tf
+    answer = point_on_line_tf(point1, point2, input)
+    assert answer == expected
