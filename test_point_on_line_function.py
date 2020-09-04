6f837aaa0885e5d1cc726b0c4a7fc2917c055e16
Xiang Dai <dino@DinodeMBP.lan1>
2020-09-04 15:38:25 -0400

    change the test case, correct the input problem

--- test_point_on_line_function.py
+++ test_point_on_line_function.py
@@ -1,10 +1,12 @@
 # test_point_on_line_function
-@pytest.mark.parametrize("point1, point2, expected", [
-	((1, 1), (2, 2), (3, 3)),
-	((1, 0), (1, 2), (1, 3)),
-	((-1, 0), (0, 1), (1, 2)),
+@pytest.mark.parametrize("point1, point2, input, expected", [
+	((1, 1), (2, 2), 3, 3),
+	((1, 0), (1, 2), 1, 3),
+	((-1, 0), (0, 1), 1, 2),
+	((0, 1), (0, 2), 0, "NaN"),
+	((1, 0), (2, 0), 4, 4)
 ])
-def test_point_on_point(point1, point2, expected):
+def test_point_on_point(point1, point2, input, expected):
 	from test_point_on_line_function import point_on_line
-	answer = point_on_line(point1, point2, expected)
+	answer = point_on_line(point1, point2, input)
 	assert answer == expected
